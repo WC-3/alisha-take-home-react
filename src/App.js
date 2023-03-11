95c991e537ea5bef90424c76dd0fd1131d1de5aa
akb3y <alisha.barraw@gmail.com>
2023-03-11 14:43:06 -0700

    create app skeleton

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import './App.css';
 
 function App() {
   return (
+    // contains global styles / modes
     <div className="App">
- 
+      App
+      {/* // contains page positioning with grid and flexbox */}
+      <div className="layout" style={{border: "3px solid red"}}>
+          Layout
+          {/* start Header */}
+            <div className="header" style={{border: "3px solid green"}}>
+              Header
+            </div>
+          {/* end Header */}
+          {/* start View */}
+          <div className="view" style={{border: "3px solid purple"}}>
+            View
+            {/* start Add */}
+            <div className="add" style={{border: "3px solid orange"}}>
+              Add
+            </div>
+            {/* end Add */}
+            {/* start List */}
+            <div className="list" style={{border: "3px solid brown"}}>
+              List
+            </div>
+            {/* end List */}
+          </div>
+          {/* end View */}
+        </div>
     </div>
   );
 }
