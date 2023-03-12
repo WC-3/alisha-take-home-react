7035838b0552261745e2998f6590ee2dee62e8ff
akb3y <alisha.barraw@gmail.com>
2023-03-12 16:51:39 -0600

    updated row

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { RiDeleteBack2Line } from 'react-icons/ri'
+import { RxUpdate } from 'react-icons/rx'
 
 function Row({person}){
 
   return(
-   <div id="parentPerson">
-    <div className="childPerson">{person[1].name}</div>
-    <div className="childPerson">{person[1].address}</div>
-    <div className="childPerson">{person[1].state}</div>
-    <div className="childPerson">{person[1].zip}</div>
-    <div className="childPerson">{person[1].age}</div>
-    <br /></div>
+    <>
+   <div className="list-row">
+    <div className="list-row-cell name">{person[1].name}</div>
+    <div className="list-row-cell address">{person[1].address}</div>
+    <div className="list-row-cell state">{person[1].state}</div>
+    <div className="list-row-cell zip">{person[1].zip}</div>
+    <div className="list-row-cell age">{person[1].age}</div>
+    <RxUpdate className='icon'/>
+    <RiDeleteBack2Line className='icon' />
+    </div>
+    </>
   )
 }
 
