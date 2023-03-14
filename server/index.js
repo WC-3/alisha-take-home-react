d0a12722dc9e0328a7738e37d044b43d35035151
akb3y <alisha.barraw@gmail.com>
2023-03-13 18:13:58 -0600

    updated data

--- server/index.js
+++ server/index.js
@@ -1,17 +1,56 @@
-const http = require('node:http');
 const express = require('express')
 const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.send('Hello World!')
+app.get('/personnel', (req, res) => {
+  console.log('getting /personnel...')
+  res.status(200)
+  res.send('getting /personnel...')
 })
 
+app.get('/personnel/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(`getting /personnel/${id}`)
+  res.status(200)
+  res.send(`getting /personnel/${id}`)
+})
+
+app.get('/personnel/:name', (req, res) => {
+  const name = req.params.name;
+  console.log(`getting /personnel/${name}`)
+  res.status(200)
+  res.send(`getting /personnel/${name}`)
+})
+
+app.get('/personnel/:age', (req, res) => {
+  const age = req.params.age;
+  console.log(`getting /personnel/${age}`)
+  res.status(200)
+  res.send(`getting /personnel/${age}`)
+})
+
+app.delete('/personnel/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(`getting /personnel/${id}`)
+  res.status(200)
+  res.send(`getting /personnel/${id}`)
+})
+
+app.put('/personnel', (req, res) => {
+  const id = req.params.id;
+  console.log(`getting /personnel/${id}`)
+  res.status(200)
+  res.send(`getting /personnel/${id}`)
+})
+
+app.post('/personnel', (req, res) => {
+  res.status(200)
+  res.send("POST Request Called")
+})
+
+
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server listening on port ${hostname}:${port}`)
 })
